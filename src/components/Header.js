ace264fedb42f0ae1f0ffd5847ae63e64106de1c
axpalx <axpalx@gmail.com>
2020-04-24 16:00:22 -0300

    Header.css removido

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Header.css';
+
 import logo from "../img/logo.png"
 import IconeCarrinho from '../img/shopping_cart.svg'
 import styled from "styled-components"
